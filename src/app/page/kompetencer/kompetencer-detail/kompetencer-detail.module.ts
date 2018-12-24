4f165c877d15c76a37fc4a0fdd36557c93c35eb7
Roman oliynyk <oliynykroma@live.com>
2018-12-24 12:55:56 +0200

    made kompetencer list

--- src/app/page/kompetencer/kompetencer-detail/kompetencer-detail.module.ts
+++ src/app/page/kompetencer/kompetencer-detail/kompetencer-detail.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { KompetencerDetailRoutingModule } from './kompetencer-detail-routing.module';
 import { KompetencerDetailComponent } from './kompetencer-detail.component';
+import {ArticleModule} from "../../../modules/article/article.module";
+import {MainTitleModule} from "../../../modules/main-title/main-title.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    KompetencerDetailRoutingModule
+    KompetencerDetailRoutingModule,
+    ArticleModule,
+    MainTitleModule
   ],
   declarations: [KompetencerDetailComponent]
 })
